d412a3afe0104ea6d67fa1aa5d095a996894e36b
George.S <piligrimius@mail.ru>
2022-08-22 13:04:21 +0300

    Task_60 Completed

---
+++ Task_60/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// массив размером 2 x 2 x 2
+// 12(0,0,0) 22(0,0,1)
+// 45(1,0,0) 53(1,0,1)
+
+
+void Matrix3D(int[,,] matrix3D)
+{
+    int[] temp = new int[matrix3D.GetLength(0) * matrix3D.GetLength(1) * matrix3D.GetLength(2)];
+    int number;
+    for (int i = 0; i < temp.GetLength(0); i++)
+    {
+        temp[i] = new Random().Next(10, 100);
+        number = temp[i];
+        if (i >= 1)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                while (temp[i] == temp[j])
+                {
+                    temp[i] = new Random().Next(10, 100);
+                    j = 0;
+                    number = temp[i];
+                }
+                number = temp[i];
+            }
+        }
+    }
+
+    int count = 0;
+    for (int x = 0; x < matrix3D.GetLength(0); x++)
+    {
+        for (int y = 0; y < matrix3D.GetLength(1); y++)
+        {
+            for (int z = 0; z < matrix3D.GetLength(2); z++)
+            {
+                matrix3D[x, y, z] = temp[count];
+                count++;
+            }
+        }
+    }
+}
+
+void PrintMatrix3D(int[,,] matrix3D)
+{
+    for (int i = 0; i < matrix3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix3D.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix3D.GetLength(2); k++)
+            {
+                Console.Write(matrix3D[i, j, k]);
+                Console.Write("({0},{1},{2}) ", i, j, k);
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+    }
+}
+
+int x = 2; int y = 2; int z = 2;
+Console.WriteLine();
+
+int[,,] matrix3D = new int[x, y, z];
+Matrix3D(matrix3D);
+PrintMatrix3D(matrix3D);
